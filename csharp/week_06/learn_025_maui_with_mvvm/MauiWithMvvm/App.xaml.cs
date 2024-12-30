2dd506a20b4bbbecababc8b18c1ffbfdffdced9c
Slisk Lindqvist <slisk.lindqvist@querzion.com>
2024-12-30 16:34:59 +0100

    MAUI with MVVM project solution created. Contacts List

---
+++ csharp/week_06/learn_025_maui_with_mvvm/MauiWithMvvm/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiWithMvvm
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
